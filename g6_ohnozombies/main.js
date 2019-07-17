c9ba95956ee9d4946d4bcfc6cac684360a5dceb1
KimKim Horas <kimkim4@gmail.com>
2019-07-17 16:46:02 -0700

    updated main.js

---
+++ g6_ohnozombies/main.js
@@ -0,0 +1,21 @@
+$(document).ready(initializeApp);
+var player1;
+var random;
+
+function initializeApp() {
+  player1 = new Human();
+  var buttonDice = $(".dice")
+  $(".boxDice").append(buttonDice);
+  buttonDice.click(rollDice);
+}
+
+function rollDice() {
+  random = Math.floor(Math.random() * 6 + 1);
+  displayDice();
+}
+
+function displayDice() {
+  var randomNumber = $("#randomNum").text(random);
+
+  $("#randomNum").append(randomNumber);
+}
